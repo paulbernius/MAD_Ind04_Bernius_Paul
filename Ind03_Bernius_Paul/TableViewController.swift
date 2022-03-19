0d810aed68925af9a1e495b4eab04f697c63288a
Paul Bernius <paul.bernius@okstate.edu>
2022-03-19 16:24:21 -0500

    Second View Controller set up. Segue partially set up

--- Ind03_Bernius_Paul/TableViewController.swift
+++ Ind03_Bernius_Paul/TableViewController.swift
@@ -81,14 +81,22 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let selectDestination = segue.destination as! ViewController
+        
+        
+        
+        selectDestination.myFlag = "Oklahoma-Flag"
+        selectDestination.myMap = "Oklahoma-Map"
+        selectDestination.myStateName = "Oklahoma"
+        selectDestination.mySquareMiles = "100"
     }
-    */
+    
 
 }
--- Ind03_Bernius_Paul/ViewController.swift
+++ Ind03_Bernius_Paul/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Flag: UIImageView!
+    var myFlag = ""
+    
+    @IBOutlet weak var Map: UIImageView!
+    var myMap = ""
+    
+    @IBOutlet weak var StateName: UILabel!
+    var myStateName = ""
+    
+    @IBOutlet weak var SquareMiles: UILabel!
+    var mySquareMiles = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Flag.image = UIImage(named: myFlag)
+        Map.image = UIImage(named: myMap)
+        StateName.text = myStateName
+        SquareMiles.text = mySquareMiles
     }
 
 
