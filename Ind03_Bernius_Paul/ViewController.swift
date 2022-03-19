5b5ce974859e1047e54d1699b9756577ac661073
Paul Bernius <paul.bernius@okstate.edu>
2022-03-19 14:24:05 -0500

    Initial Commit

---
+++ Ind03_Bernius_Paul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind03_Bernius_Paul
+//
+//  Created by Paul Bernius on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
