a217c7d8c6280e73e7b44c3e4b39ac49e919ec96
Paul Bernius <paul.bernius@okstate.edu>
2022-04-05 23:06:21 -0500

    Final touches

--- Ind03_Bernius_Paul/Util.swift
+++ Ind03_Bernius_Paul/Util.swift
@@ -11,6 +11,22 @@ fileprivate var aView : UIView?
 
 extension UIViewController {
     
+    // This two methods are referenced in "Reference 1"
+    
+    
+    /*
+     This swift file extends UIViewController to add
+     the spinner function to all view controllers.
+     This way, inside any view controller, I can type
+     self.showSpinner() and the spinner will start
+     animating. Then, I can type, self.removeSpinner
+     to remove the spinner.
+     
+     Again, because I extended UIViewController, these
+     two functions can be used in any view controller.
+     */
+    
+    // Method to show the spinner
     func showSpinner() {
         aView = UIView(frame: self.view.bounds)
         let ai = UIActivityIndicatorView(style: .large)
@@ -20,6 +36,7 @@ extension UIViewController {
         self.view.addSubview(aView!)
     }
     
+    // Method to remove the spinner
     func removeSpinner() {
         aView?.removeFromSuperview()
         aView = nil
